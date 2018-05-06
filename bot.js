e7742a471073d8cd9afbd6e0e8d6f870d74da875
MrLegend-Robby <35876269+MrLegend-Robby@users.noreply.github.com>
2018-05-06 10:29:40 +0300

    bot2

--- bot.js
+++ bot.js
@@ -66,4 +66,4 @@ bott.on('message', (message) => {
     }
 });
  
-process.env.NDQwNDQ0NTY3NTMyMDc3MDU2.Dc8dWA.z0J0cwVgnBxzxL55mpTVr4efId
+bott.login(process.env.NDQwNDQ0NTY3NTMyMDc3MDU2.DdA_cA.mjjCaoiGkgW5Thi83iaDPASWgIc)
