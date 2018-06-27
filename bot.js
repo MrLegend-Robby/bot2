d85859216f936089823ffe082d2bade48fb9bf3d
MrLegend-Robby <35876269+MrLegend-Robby@users.noreply.github.com>
2018-06-27 23:08:18 +0300

    bot2

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const prefix = '*';
 const colors = ['92ce14', '4bd588', '4bd5b9', 'd54b76', '00edff', '0077ff', 'f2ff00', 'cbd073', 'd09573', 'f2070b',];
 function color () {
   colors.forEach(function (item, number) {
-      setTimeout(function () {bott.guilds.get('432926249006923806').roles.get('441148168730574858').setColor(item).catch();if(number === colors.length-1) setTimeout(function () {color()}, 1000)}, number*1000);
+      setTimeout(function () {bott.guilds.get('456532005580963850').roles.get('456533542743048192').setColor(item).catch();if(number === colors.length-1) setTimeout(function () {color()}, 1000)}, number*1000);
   });
 }
  
