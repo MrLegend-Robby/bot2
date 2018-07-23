561030063882ca7f264ebb99e78a099e3b71753f
MrLegend-Robby <35876269+MrLegend-Robby@users.noreply.github.com>
2018-07-23 23:08:44 +0300

    bot2

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const prefix = '*';
 const colors = ['92ce14', '4bd588', '4bd5b9', 'd54b76', '00edff', '0077ff', 'f2ff00', 'cbd073', 'd09573', 'f2070b',];
 function color () {
   colors.forEach(function (item, number) {
-      setTimeout(function () {bott.guilds.get('466333714649972758').roles.get('470694094751006732').setColor(item).catch();if(number === colors.length-1) setTimeout(function () {color()}, 1000)}, number*1000);
+      setTimeout(function () {bott.guilds.get('466333714649972758').roles.get('470532429912735744').setColor(item).catch();if(number === colors.length-1) setTimeout(function () {color()}, 1000)}, number*1000);
   });
 }
  
